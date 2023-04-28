c01b8ef8770654839a4beb3be16e5b040ae9c7d0
Atanas Stankin <a_stankin@abv.bg>
2023-04-28 16:38:56 +0300

    Update

--- traning/student_management/student_management/students/urls.py
+++ traning/student_management/student_management/students/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('student/<int:id>', views.view_student, name='view_student'),
-    path('search_student', views.search_student, name='search_student'),
-    path('all_students', views.all_students, name='all_students'),
+    path('search_student/', views.search_student, name='search_student'),
+    path('all_students/', views.all_students, name='all_students'),
+    path('add/', views.add, name='add'),
 ]
