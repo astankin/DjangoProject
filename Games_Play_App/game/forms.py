74586daa3fe3305c3d77cb66c001c04181280602
Atanas Stankin <a_stankin@abv.bg>
2023-05-26 16:47:36 +0300

    Uploaded MyMusicApp

--- Games_Play_App/game/forms.py
+++ Games_Play_App/game/forms.py
@@ -16,6 +16,6 @@ class GameForm(forms.ModelForm):
 class GameDeleteForm(GameForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for (_, field) in self.fields.items():
+        for field in self.fields.values():
             field.widget.attrs['disabled'] = 'disabled'
             field.widget.attrs['readonly'] = 'readonly'
