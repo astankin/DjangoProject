9fe1429656969a329ea2e3a86439f0419600d7be
Atanas Stankin <a_stankin@abv.bg>
2023-06-09 11:55:39 +0300

    Update

--- Car_Collection_App/Car_Collection_App/profile_app/views.py
+++ Car_Collection_App/Car_Collection_App/profile_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
-from Car_Collection_App.profile_app.forms import ProfileForm
+from Car_Collection_App.car_app.models import CarModel
+from Car_Collection_App.profile_app.forms import ProfileForm, ProfileEditForm
 from Car_Collection_App.profile_app.models import ProfileModel
 
 
@@ -21,12 +22,38 @@ def profile_create(request):
 
 
 def profile_details(request):
-    return render(request, 'profile-details.html')
+    profile = ProfileModel.objects.first()
+    cars = CarModel.objects.all()
+    total_price = sum([car.price for car in cars])
+    context = {
+        'profile': profile,
+        'total_price': total_price,
+
+    }
+    return render(request, 'profile-details.html', context)
 
 
 def profile_edit(request):
-    return render(request, 'profile-edit.html')
+    profile = ProfileModel.objects.first()
+    if request.method == 'POST':
+        form = ProfileEditForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile-details')
+    else:
+        form = ProfileEditForm(instance=profile)
+    context = {
+        'form': form,
+        'profile': profile,
+    }
+    return render(request, 'profile-edit.html', context)
 
 
 def profile_delete(request):
+    profile = ProfileModel.objects.first()
+    cars = CarModel.objects.all()
+    if request.method == 'POST':
+        profile.delete()
+        cars.delete()
+        return redirect('index')
     return render(request, 'profile-delete.html')
